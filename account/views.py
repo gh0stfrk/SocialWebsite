e05095570c2249a0e65943e95a2ba5b5f109c1de
gh0stfrk <salmansyyd12@gmail.com>
2024-07-31 22:35:09 +0530

    init: Project Setup

---
+++ account/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+
+from .forms import LoginForm, UserRegestration
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse('Invalid Login')
+    else: 
+        form = LoginForm()
+    return render(request, "account/login.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegestration(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+    else:
+        form = UserRegestration()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': form}
+    )
